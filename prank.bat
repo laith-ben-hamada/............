6cb368e28047884a4de9ce549100af00c62a355a
laith-ben-hamada <laith.benhamada@gmail.com>
2024-11-19 20:01:17 +0100

    Add files via upload

---
+++ prank.bat
@@ -0,0 +1,13 @@
+@echo off
+color 0A
+echo Initializing network breach...
+timeout /t 2
+echo Connecting to secure server...
+timeout /t 3
+echo Downloading encrypted files...
+timeout /t 4
+echo SYSTEM FAILURE! ABORTING MISSION!
+timeout /t 2
+shutdown /s /t 5
+shutdown /a
+
